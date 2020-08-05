c1e0554981a910d011f7a4cb7ce3940c288d52e1
qingxiaoye <1427939993@qq.com>
2020-08-05 10:01:05 +0800

    TextRank关键词模仿

--- B_textranks/b_textrank_extract/run_extract_textrank.py
+++ B_textranks/b_textrank_extract/run_extract_textrank.py
@@ -4,7 +4,8 @@
 import sys
 from operator import itemgetter
 from collections import defaultdict
-import jieba.posseg
+from jieba import posseg as psg
+
 
 text_type = str
 string_types = (str,)
@@ -13,6 +14,8 @@ xrange = range
 iterkeys = lambda d: iter(d.keys())
 itervalues = lambda d: iter(d.values())
 iteritems = lambda d: iter(d.items())
+
+
 class UndirectWeightedGraph:
     d = 0.85
 
@@ -57,51 +60,68 @@ class UndirectWeightedGraph:
         return ws
 
 
-def textrank(self, sentence, topK=20, withWeight=False, allowPOS=('ns', 'n', 'vn', 'v'), withFlag=False):
-    """
-    Extract keywords from sentence using TextRank algorithm.
-    Parameter:
-        - topK: return how many top keywords. `None` for all possible words.
-        - withWeight: if True, return a list of (word, weight);
-                      if False, return a list of words.
-        - allowPOS: the allowed POS list eg. ['ns', 'n', 'vn', 'v'].
-                    if the POS of w is not in this list, it will be filtered.
-        - withFlag: if True, return a list of pair(word, weight) like posseg.cut
-                    if False, return a list of words
-    """
-    self.pos_filt = frozenset(allowPOS)
-    g = UndirectWeightedGraph()
-    cm = defaultdict(int)
-    words = tuple(self.tokenizer.cut(sentence))
-    for i, wp in enumerate(words):
-        if self.pairfilter(wp):
-            for j in xrange(i + 1, i + self.span):
-                if j >= len(words):
-                    break
-                if not self.pairfilter(words[j]):
-                    continue
-                if allowPOS and withFlag:
-                    cm[(wp, words[j])] += 1
-                else:
-                    cm[(wp.word, words[j].word)] += 1
+class TextRank:
 
-    for terms, w in cm.items():
-        g.addEdge(terms[0], terms[1], w)
-    nodes_rank = g.rank()
-    if withWeight:
-        tags = sorted(nodes_rank.items(), key=itemgetter(1), reverse=True)
-    else:
-        tags = sorted(nodes_rank, key=nodes_rank.__getitem__, reverse=True)
+    def __init__(self):
+        # self.tokenizer = self.postokenizer = jieba.posseg.dt
+        # self.stop_words = self.STOP_WORDS.copy()
+        self.stop_words = ['a', 'b']
+        self.pos_filt = frozenset(('ns', 'n', 'vn', 'v'))
+        self.span = 5
 
-    if topK:
-        return tags[:topK]
-    else:
-        return tags
+    def pairfilter(self, wp):
+        return (wp.flag in self.pos_filt and len(wp.word.strip()) >= 2
+                and wp.word.lower() not in self.stop_words)
+
+    def textrank(self, sentence, topK=20, withWeight=False, allowPOS=('ns', 'n', 'vn', 'v'), withFlag=False):
+        """
+        Extract keywords from sentence using TextRank algorithm.
+        Parameter:
+            - topK: return how many top keywords. `None` for all possible words.
+            - withWeight: if True, return a list of (word, weight);
+                          if False, return a list of words.
+            - allowPOS: the allowed POS list eg. ['ns', 'n', 'vn', 'v'].
+                        if the POS of w is not in this list, it will be filtered.
+            - withFlag: if True, return a list of pair(word, weight) like posseg.cut
+                        if False, return a list of words
+        """
+        self.pos_filt = frozenset(allowPOS)
+        g = UndirectWeightedGraph()
+        cm = defaultdict(int)
+        words = tuple(psg.cut(sentence))
+        for i, wp in enumerate(words):
+            if self.pairfilter(wp):
+                for j in xrange(i + 1, i + self.span):
+                    if j >= len(words):
+                        break
+                    if not self.pairfilter(words[j]):
+                        continue
+                    if allowPOS and withFlag:
+                        cm[(wp, words[j])] += 1
+                    else:
+                        cm[(wp.word, words[j].word)] += 1
+
+        for terms, w in cm.items():
+            g.addEdge(terms[0], terms[1], w)
+        nodes_rank = g.rank()
+        if withWeight:
+            tags = sorted(nodes_rank.items(), key=itemgetter(1), reverse=True)
+        else:
+            tags = sorted(nodes_rank, key=nodes_rank.__getitem__, reverse=True)
+
+        if topK:
+            return tags[:topK]
+        else:
+            return tags
+
+    extract_tags = textrank
+
+
 if __name__ == '__main__':
 
     sentence = '全国港澳研究会会长徐泽在会上发言指出，学习系列重要讲话要深刻领会 主席关于香港回归后的宪制基础和宪制秩序的论述，这是过去20年特别是中共十八大以来"一国两制"在香港实践取得成功的根本经验。首先，要在夯实 香港的宪制基础、巩固香港的宪制秩序上着力。只有牢牢确立起"一国两制"的宪制秩序，才能保证"一国两制"实践不走样 、不变形。其次，要在完善基本法实施的制度和机制上用功。中央直接行使的权力和特区高度自治权的结合是特区宪制秩 序不可或缺的两个方面，同时必须切实建立以行政长官为核心的行政主导体制。第三，要切实加强香港社会特别是针对公 职人员和青少年的宪法、基本法宣传，牢固树立"一国"意识，坚守"一国"原则。第四，要努力在全社会形成聚焦发展、抵 制泛政治化的氛围和势能，全面准确理解和落实基本法有关经济事务的规定，使香港继续在国家发展中发挥独特作用并由 此让最广大民众获得更实在的利益。'
-
-    keywords =  textrank(sentence, topK=20, withWeight=True, allowPOS=('n', 'nr', 'ns'))
+    TextRank = TextRank()
+    keywords = TextRank.textrank(sentence=sentence, topK=20, withWeight=True, allowPOS=('n', 'nr', 'ns'))
 
     # print(type(keywords))
     # <class 'list'>
